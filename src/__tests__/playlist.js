463c65463a12409fc5a9f879abbc227175b1cc2e
Dowen Robinson <drobinson@ad.accelclaims.com>
2020-06-20 09:04:42 -0500

    Added svgs for player comp and added global styles

--- src/__tests__/playlist.js
+++ src/__tests__/playlist.js
@@ -2,12 +2,6 @@ import React from 'react'
 import Playlist from '../components/navigator/pages/playlist'
 import { shallow } from 'enzyme'
 
-jest.mock('react-router-dom', () => ({
-    useParams: () => ({
-        id: 'MOCKED_VALUE',
-    }),
-}))
-
 test('renders without crashing', () => {
     shallow(<Playlist />)
 })
