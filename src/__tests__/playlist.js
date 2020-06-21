58aa0f7c9c70902f90abd9b7311c6da10ca222df
Dowen Robinson <drobinson@ad.accelclaims.com>
2020-06-20 19:02:06 -0500

    Updated playlist test

--- src/__tests__/playlist.js
+++ src/__tests__/playlist.js
@@ -2,6 +2,12 @@ import React from 'react'
 import Playlist from '../components/navigator/pages/playlist'
 import { shallow } from 'enzyme'
 
+jest.mock('react-router-dom', () => ({
+    useParams: () => ({
+        id: 'MOCKED_VALUE',
+    }),
+}))
+
 test('renders without crashing', () => {
     shallow(<Playlist />)
 })
