39703f8f1bcd7eac01cd1916e75270a490e7053f
Dowen Robinson <drobinson@ad.accelclaims.com>
2020-07-01 23:27:02 -0500

    Updated library test

--- src/__tests__/library.js
+++ src/__tests__/library.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import Library from '../components/navigator/pages/library'
 import { shallow } from 'enzyme'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+const mockStore = configureMockStore([thunk])
 
 /* SMOKE TEST */
 test('renders without crashing', () => {
-    shallow(<Library />)
+    const store = mockStore({
+        startup: false,
+    })
+    shallow(
+        <Provider store={store}>
+            <Library />
+        </Provider>
+    )
 })
